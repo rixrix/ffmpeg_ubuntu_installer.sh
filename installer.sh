2c366a51b7658bd5484040ce5e450ea2d9726fd5
Richard Sentino <richard@mindginative.com>
2016-11-28 01:58:18 +1300

    chore(libs): enable all scripts

--- installer.sh
+++ installer.sh
@@ -11,14 +11,14 @@ export FFMPEG_BIN_DIR="$HOME/.ffmpeg_bin"
 
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" # source http://stackoverflow.com/a/246128
 
-# $CURRENT_DIR/dependencies.sh
-# $CURRENT_DIR/yasm.sh
-# $CURRENT_DIR/libx264.sh
-# $CURRENT_DIR/libx265.sh
-# $CURRENT_DIR/libfdk-aac.sh
-# $CURRENT_DIR/libmp3lame.sh
-# $CURRENT_DIR/libopus.sh
-# $CURRENT_DIR/libvpx.sh
+$CURRENT_DIR/dependencies.sh
+$CURRENT_DIR/yasm.sh
+$CURRENT_DIR/libx264.sh
+$CURRENT_DIR/libx265.sh
+$CURRENT_DIR/libfdk-aac.sh
+$CURRENT_DIR/libmp3lame.sh
+$CURRENT_DIR/libopus.sh
+$CURRENT_DIR/libvpx.sh
 $CURRENT_DIR/ffmpeg.sh
 
 echo "FFmpeg installed!"
